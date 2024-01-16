180fac4627799a53def06e72b23b76247a0414a6
mdrahathossenantor <md.rahathossenantor@gmail.com>
2024-01-16 15:40:51 +0600

    Footer section completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <footer className="py-6 md:py-8">
+            <div className="container mx-auto">
+                <p className="text-center text-base text-gray-500">
+                    Copyright &copy;2024 | All rights reserved by Md Rahat Hossen Antor
+                </p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
