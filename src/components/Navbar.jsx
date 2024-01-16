b33e791f149b7386eea51d197ca713949b9e4b32
mdrahathossenantor <md.rahathossenantor@gmail.com>
2024-01-16 15:36:17 +0600

    Hero section completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/lws-logo-en.svg";
 
 const Navbar = () => {
     return (
-        <nav className="py-6">
+        <nav className="py-6 mx-5">
             <div className="container mx-auto flex items-center justify-between gap-x-6">
                 <a href="/">
                     <img className="h-[40px]" src={logo} alt="logo" />
